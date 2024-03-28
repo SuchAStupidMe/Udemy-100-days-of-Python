075ae75047281594d9a6cd30831235e6e285470e
SuchAStupidMe <74260728+SuchAStupidMe@users.noreply.github.com>
2024-03-28 12:39:50 +0200

    Add files via upload

--- 26_day/main.py
+++ 26_day/main.py
@@ -6,5 +6,9 @@ nato_csv = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in nato_csv.iterrows()}
 print(nato_dict)
 
-user_word = input('Type your word: ')
-print([nato_dict[letter.capitalize()] for letter in user_word])
+while True:
+    user_word = input('Type your word: ')
+    try:
+        print([nato_dict[letter.capitalize()] for letter in user_word])
+    except KeyError:
+        print('Sorry, only letters are allowed')
